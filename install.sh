5e3d87eb8b7b7d03bb08bd0d20b0ddb6d48b54f5
John Mutua Makumi <johnmutuamakumi1@gmail.com>
2025-06-19 22:29:08 +0300

    add install.sh file

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+REPO_URL="git@github.com:Makumiii/posh_themer.git"
+FOLDER_NAME="posh_themer"
+DENO_CMD="deno -A ~/$FOLDER_NAME/index.ts"
+
+cat <<EOF 
+-----WELCOME TO POSH_THEMER-----
+Installation will run now
+--------------------------------
+EOF
+
+echo checking if already installed
+cd ~ || exit
+
+install(){
+    git clone $REPO_URL
+
+    cat <<EOF >> ~/.zshrc 
+##added by posh_themer_installer
+function posh_theme(){
+    $DENO_CMD && source ~/.zshrc
+}
+posh_theme
+
+EOF
+    
+}
+
+if [ -d ~/"$FOLDER_NAME" ]; then
+    echo Already installed
+
+    echo Remove existing folder at ~/posh_themer
+
+else
+    echo Proceeding with installation
+    install
+fi
+
+
+
+
